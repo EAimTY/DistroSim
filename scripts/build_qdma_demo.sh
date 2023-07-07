a7f5289bfe1fb079a1a67c09cecf7c3818ca64e4
EAimTY <ea.imty@gmail.com>
2023-07-07 17:31:38 +0900

    fix bug getting project dir

--- scripts/build_qdma_demo.sh
+++ scripts/build_qdma_demo.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-RUN_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../run
-
+GIT_DIR=$( cd $(dirname $0)/..; pwd )
+RUN_DIR=${GIT_DIR}/run
 mkdir -p ${RUN_DIR}
-cd ${RUN_DIR/..}
+
+cd ${GIT_DIR}
 git submodule update --init libsystemctlm-soc
 git submodule update --init pcie-model
 cat <<EOF > .config.mk
 SYSTEMC = ${RUN_DIR}/systemc-2.3.3
 EOF
 cat .config.mk
-make pcie/versal/cpm5-qdma-demo pcie/versal/cpm4-qdma-demo
+make pcie/versal/cpm5-qdma-demo pcie/versal/cpm4-qdma-democ
--- scripts/install_systemc.sh
+++ scripts/install_systemc.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-RUN_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../run
-
+GIT_DIR=$( cd $(dirname $0)/..; pwd )
+RUN_DIR=${GIT_DIR}/run
 mkdir -p ${RUN_DIR}
+
 cd ${RUN_DIR}
 wget -q https://www.accellera.org/images/downloads/standards/systemc/systemc-2.3.3.tar.gz && tar xzf systemc-2.3.3.tar.gz
 cd systemc-2.3.3
