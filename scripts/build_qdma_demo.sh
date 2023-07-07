128eb7df140da2c188cac3d4f95f1de55dc26bff
EAimTY <ea.imty@gmail.com>
2023-07-07 17:44:18 +0900

    only making cpm5-qdma-demo

--- scripts/build_qdma_demo.sh
+++ scripts/build_qdma_demo.sh
@@ -8,7 +8,7 @@ cd ${GIT_DIR}
 git submodule update --init libsystemctlm-soc
 git submodule update --init pcie-model
 cat <<EOF > .config.mk
-SYSTEMC = ${RUN_DIR}/systemc-2.3.3
+SYSTEMC=${RUN_DIR}/systemc-2.3.3
 EOF
 cat .config.mk
-make pcie/versal/cpm5-qdma-demo pcie/versal/cpm4-qdma-democ
+make pcie/versal/cpm5-qdma-demo
