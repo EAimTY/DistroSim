43e2177d1dbbd1793de471705487bdb080a1ae5b
EAimTY <ea.imty@gmail.com>
2023-07-07 18:18:18 +0900

    add dma_driver build script

---
+++ scripts/build_dma_driver.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+GIT_DIR=${cd $(dirname $0)/..; pwd}
+RUN_DIR=${GIT_DIR}/run
+mkdir -p ${RUN_DIR}
+
+# dependencies: git make gcc libaio1 libaio-dev kmod linux-headers-generic
+
+cd ${RUN_DIR}
+git clone https://github.com/Xilinx/dma_ip_drivers.git
+cd dma_ip_drivers/QDMA/linux-kernel
+make TANDEM_BOOT_SUPPORTED=1 KDIR=/usr/src/linux-headers-${uname -r}
