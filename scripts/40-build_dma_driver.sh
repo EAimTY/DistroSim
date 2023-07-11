3b21eeff0c82af070901ebf89c3bb03b57508f74
EAimTY <ea.imty@gmail.com>
2023-07-12 02:00:49 +0900

    copy built driver to TEMP_FILE_PATH

--- scripts/40-build_dma_driver.sh
+++ scripts/40-build_dma_driver.sh
@@ -7,6 +7,12 @@ mkdir -p ${RUN_DIR}
 # dependencies: git make gcc libaio1 libaio-dev kmod linux-headers-generic
 
 cd ${RUN_DIR}
+
+TEMP_FILE_PATH="${RUN_DIR}/tmp"
+
 git clone https://github.com/Xilinx/dma_ip_drivers.git
 cd dma_ip_drivers/QDMA/linux-kernel
 make TANDEM_BOOT_SUPPORTED=1 KDIR=/usr/src/linux-headers-$(uname -r)
+
+mkdir -p $TEMP_FILE_PATH
+mv dma_ip_drivers $TEMP_FILE_PATH
