80e415f870837179fd0be108f32a6062f638d63a
EAimTY <ea.imty@gmail.com>
2023-07-12 02:04:50 +0900

    copy driver instead of move

--- scripts/40-build_dma_driver.sh
+++ scripts/40-build_dma_driver.sh
@@ -15,4 +15,5 @@ cd dma_ip_drivers/QDMA/linux-kernel
 make TANDEM_BOOT_SUPPORTED=1 KDIR=/usr/src/linux-headers-$(uname -r)
 
 mkdir -p $TEMP_FILE_PATH
-mv dma_ip_drivers $TEMP_FILE_PATH
+rm -rf $TEMP_FILE_PATH/dma_ip_drivers
+cp dma_ip_drivers $TEMP_FILE_PATH
