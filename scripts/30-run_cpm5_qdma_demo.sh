ad3e3e65d56a4e7378964b98dc0ee072a96f0c99
EAimTY <ea.imty@gmail.com>
2023-07-12 09:59:21 +0900

    update unix socket location

--- scripts/30-run_cpm5_qdma_demo.sh
+++ scripts/30-run_cpm5_qdma_demo.sh
@@ -4,4 +4,7 @@ GIT_DIR=$(cd $(dirname $0)/..; pwd)
 RUN_DIR=${GIT_DIR}/run
 mkdir -p ${RUN_DIR}
 
-LD_LIBRARY_PATH=${RUN_DIR}/systemc-2.3.3/lib-linux64/ ./pcie/versal/cpm5-qdma-demo unix:${RUN_DIR}/qemu-rport-_machine_peripheral_rp0_rp 10000 &
+TEMP_FILE_PATH="${RUN_DIR}/tmp"
+mkdir -p $TEMP_FILE_PATH
+
+LD_LIBRARY_PATH=${RUN_DIR}/systemc-2.3.3/lib-linux64/ ./pcie/versal/cpm5-qdma-demo unix:${TEMP_FILE_PATH}/qemu-rport-_machine_peripheral_rp0_rp 10000 &
