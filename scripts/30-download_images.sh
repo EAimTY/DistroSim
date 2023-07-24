9b7c40406943acfe42673466d9f686f2a6ecd1ff
EAimTY <EAimTY@users.noreply.github.com>
2023-07-24 17:21:02 +0900

    download ubuntu cloud images from mirror

--- scripts/30-download_images.sh
+++ scripts/30-download_images.sh
@@ -9,17 +9,17 @@ mkdir -p ${RUN_DIR}
 
 cd ${RUN_DIR}
 
-RELEASE_URL="https://cloud-images.ubuntu.com/releases/focal/release"
 IMG_SIZE="10G"
-UBUNTU_IMG_PATH="${RUN_DIR}/ubuntu-20.04-server-cloudimg-amd64.img"
+RELEASE_URL="https://mirrors.tuna.tsinghua.edu.cn/ubuntu-cloud-images/focal/current"
+CLOUDIMG_NAME="focal-server-cloudimg-amd64"
 CLOUD_CONFIG_IMG_PATH="${RUN_DIR}/cloud_init.img"
-UBUNTU_KERNEL_PATH="${RUN_DIR}/ubuntu-20.04-server-cloudimg-amd64-vmlinuz-generic"
-UBUNTU_INITRD_PATH="${RUN_DIR}/ubuntu-20.04-server-cloudimg-amd64-initrd-generic"
-BIOS_PATH="${RUN_DIR}/qemu/pc-bios/bios-256k.bin"
+UBUNTU_IMG_PATH="${RUN_DIR}/${CLOUDIMG_NAME}.img"
+UBUNTU_KERNEL_PATH="${RUN_DIR}/${CLOUDIMG_NAME}-vmlinuz-generic"
+UBUNTU_INITRD_PATH="${RUN_DIR}/${CLOUDIMG_NAME}-initrd-generic"
 
-rm -rf ubuntu-20.04-server-cloudimg-amd64.img cloud_init.img ubuntu-20.04-server-cloudimg-amd64-vmlinuz-generic ubuntu-20.04-server-cloudimg-amd64-initrd-generic
-wget $RELEASE_URL/ubuntu-20.04-server-cloudimg-amd64.img
-qemu-img resize ubuntu-20.04-server-cloudimg-amd64.img $IMG_SIZE
+rm -rf cloud_init.img ${CLOUDIMG_NAME}.img ${CLOUDIMG_NAME}-vmlinuz-generic ${CLOUDIMG_NAME}-initrd-generic
+wget ${RELEASE_URL}/${CLOUDIMG_NAME}.img
+qemu-img resize ${CLOUDIMG_NAME}.img $IMG_SIZE
 cloud-localds $CLOUD_CONFIG_IMG_PATH $GIT_DIR/scripts/cloud_init.cfg
-wget $RELEASE_URL/unpacked/ubuntu-20.04-server-cloudimg-amd64-vmlinuz-generic
-wget $RELEASE_URL/unpacked/ubuntu-20.04-server-cloudimg-amd64-initrd-generic
+wget $RELEASE_URL/unpacked/${CLOUDIMG_NAME}-vmlinuz-generic
+wget $RELEASE_URL/unpacked/${CLOUDIMG_NAME}-initrd-generic
